99c22358a8c6186b227804d7bdf08022a2694d65
opeplus <opeplus4@gmail.com>
2024-02-05 01:34:40 -0800

    created a random function
    created a character label

--- pythonProject10/welcome label.py
+++ pythonProject10/welcome label.py
@@ -7,5 +7,14 @@ root.geometry("600x400")
 
 welcome_label = Label(root, text="Welcome! Click the Generate button to generate your unique password.", font=("Arial bold", 12, "bold"), bg="black", fg="white")
 welcome_label.pack(pady=10)
+def new_rand() :
+    pw_entry.delete(0, END)
+    pw_length = int(ny_entry.get())
+    ny_password = ''
+
+    for _ in range(pw_length):
+        ny_password += chr(randint(33, 126))
+
+    pw_entry.insert(0, ny_password)
 
 root.mainloop()
