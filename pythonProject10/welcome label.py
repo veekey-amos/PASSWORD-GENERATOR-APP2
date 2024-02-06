31afb0003969b86896ff09026fb56f1fa8be91c5
Marvydara <149010152+Marvydara@users.noreply.github.com>
2024-02-06 03:07:07 -0800

    Update welcome label.py

--- pythonProject10/welcome label.py
+++ pythonProject10/welcome label.py
@@ -25,7 +25,7 @@ lf.pack(pady=20)
 ny_entry = Entry(lf, font=("Arial bold", 44))
 ny_entry.pack(pady=20)
 
-pw_entry = Entry(root, text='', font=('Arial bold', 24), bd=0, bg="midnight blue")
+pw_entry = Entry(root, text='', font=('Arial bold', 24), bd="white", bg="midnight blue")
 pw_entry.pack(pady=20)
 
 ny_frame = Frame(root)
