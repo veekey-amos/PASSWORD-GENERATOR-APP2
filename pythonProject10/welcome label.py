d2795834906526c33ac6a45ebd7be4d0a3ab51e1
opeplus <147916523+opeplus@users.noreply.github.com>
2024-02-06 03:38:00 -0800

    Update welcome label.py

--- pythonProject10/welcome label.py
+++ pythonProject10/welcome label.py
@@ -31,7 +31,7 @@ pw_entry.pack(pady=20)
 ny_frame = Frame(roo, bg="midnightblue")
 ny_frame.pack(pady=20)
 
-ny_button = Button(ny_frame, text="Generate unique password", command=new_rand, )
+ny_button = Button(ny_frame, text="Generate unique password", command=new_rand, bg="black", fg= "white" )
 ny_button.grid(row=0, column=0, padx=10)
 
 root.mainloop()
